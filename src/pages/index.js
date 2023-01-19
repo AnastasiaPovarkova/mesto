e99780901433f7402bc5209ac803a74b9d21d3f9
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-01-20 01:33:21 +0300

    Создана папка src

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import { settings, popupElementEditProfile, popupElementAddCard, popupOpenButtonElement, 
-  popupAddButtonElement, popupElementOpenImage, nameInput, jobInput } from './constants.js'
-import { initialCards } from './initial-сards.js'
+  popupAddButtonElement, popupElementOpenImage, nameInput, jobInput } from '../scripts/constants.js'
+import { initialCards } from '../scripts/initial-сards.js'
 
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
+import Card from '../scripts/Card.js';
+import FormValidator from '../scripts/FormValidator.js';
 
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
+import Section from '../scripts/Section.js';
+import PopupWithImage from '../scripts/PopupWithImage.js';
+import PopupWithForm from '../scripts/PopupWithForm.js';
+import UserInfo from '../scripts/UserInfo.js';
 
 
 //Listeners 
